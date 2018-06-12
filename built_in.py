7b954d054b021eff8c5250b7b5caf635fc60b142
YOGENDRAPITALE <yogendrapitale@gmail.com>
2018-06-12 18:29:05 +0530

    Add files via upload

--- built_in.py
+++ built_in.py
@@ -1,6 +1,28 @@
 								#Built in Functions
 								
 import math
+
+print("lets do some Maths")
+print("2+2=",2+2)
+print("2+2*2=",2+2*2)
+print("2+2/2=",2+2/2)
+print("2-2=",2-2)
+print("2/2*2=",2/2*2)
+print("-2+2=",-2+2)
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print ("Is it true that 3 + 2 < 5 - 7?")
+
+print (3 + 2 < 5 - 7)
+print ("What is 3 + 2?", 3 + 2)
+
+print ("How about some more.")
+print ("Is it greater?", 5 > - 2)
+print( "Is it greater or equal?", 5 >= - 2)
+print ("Is it less or equal?", 5 <= -2)
+s=((float)(2+1.5))
+print(s)
+
 #Raise Too
 a=2**5;
 print(a)
